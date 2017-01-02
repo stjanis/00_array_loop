f7b42d0b09cdf51db97e2c74c777d40fb25d0566
stjanis <janis.stipnieks@novasol.com>
2017-01-02 14:22:34 +0100

    01: array forEach method

--- js/01_array_forEach.js
+++ js/01_array_forEach.js
@@ -1,1 +1,25 @@
+const getStockSymbols = (stocks) => {
+  let symbols = [];
 
+  // for loop solution
+  /*
+  for(let i = 0; i < stocks.length; i++) {
+    symbols.push(stocks[i].symbol);
+  }
+  */
+
+  // forEach solution (can run also asynchronously, unlike for loop)
+  stocks.forEach( stock => {
+    symbols.push(stock.symbol);
+  });
+
+  return symbols;
+};
+
+let stockSymbols = getStockSymbols([
+  { symbol: "XFX", price: 240.22, volume: 23432 },
+  { symbol: "TNZ", price: 332.19, volume: 234 },
+  { symbol: "JXJ", price: 120.22, volume: 5323 }
+]);
+
+console.log(stockSymbols);
