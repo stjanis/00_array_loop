3c6e44cbc0248911be997d1d1933b5772181674e
stjanis <janis.stipnieks@novasol.com>
2017-01-02 14:58:16 +0100

    02: array map method

---
+++ js/02_array_map.js
@@ -0,0 +1,22 @@
+const getStockSymbols = (stocks) => stocks.map(stock => stock.symbol);
+
+// for illustrative purposes, override array map method
+/*
+Array.prototype.map = function(projection) {
+  let results = [];
+  // this.forEach is being added to the Array itself.
+  // This, in the context of this function, is an Array.
+  this.forEach( item => {
+    results.push(projection(item))
+  });
+  return results;
+}
+*/
+
+let stockSymbols = getStockSymbols([
+  { symbol: "XFX", price: 240.22, volume: 23432 },
+  { symbol: "TNZ", price: 332.19, volume: 234 },
+  { symbol: "JXJ", price: 120.22, volume: 5323 }
+]);
+
+console.log(stockSymbols);
